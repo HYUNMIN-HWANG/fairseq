129d2ff72bf33243794ecaacc6906f56a7ca7a0f
Hyunmin <hwangkei0212@gmail.com>
2022-03-17 14:41:49 +0900

    print tsv

--- print_tsv.py
+++ print_tsv.py
@@ -3,19 +3,11 @@ import csv
 
 
 with open("manifest/train.tsv") as f:
-    train_data = f.read()
-    copy_train = "/output/train.tsv"
-    copy_train.write(train_data)
-
     tr = csv.reader(f, delimiter='\t')
     for row in tr:
         print(row)
 
 with open("manifest/valid.tsv") as f:
-    valid_data = f.read()
-    copy_valid = "/output/valid.tsv"
-    copy_valid.write(valid_data)
-
     tr = csv.reader(f, delimiter='\t')
     for row in tr:
     	print(row)
