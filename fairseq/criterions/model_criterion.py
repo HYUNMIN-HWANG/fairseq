1add8e32b0706246109e6e98473aeac42a4c8d88
Hyunmin <hwangkei0212@gmail.com>
2022-03-20 18:18:53 +0900

    sample size

--- fairseq/criterions/model_criterion.py
+++ fairseq/criterions/model_criterion.py
@@ -121,7 +121,7 @@ class ModelCriterion(FairseqCriterion):
         sample_size = utils.item(
             sum(log.get("sample_size", 0) for log in logging_outputs)
         )
-
+        print(loss_sum, "\t", sample_size)
         metrics.log_scalar("loss", loss_sum / sample_size, sample_size, round=3)
         metrics.log_scalar("ntokens", ntokens)
         metrics.log_scalar("nsentences", nsentences)
